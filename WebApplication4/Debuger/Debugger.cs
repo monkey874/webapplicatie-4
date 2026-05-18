6451bf98a6e683c09988a3ab42c21356c1a72e22
rickcodemake45 <504848@student.firda.nl>
2026-05-18 13:19:12 +0200

    Update: heb een zelfgemaakt Debug systeem gemaakt

--- WebApplication4/Debuger/Debugger.cs
+++ WebApplication4/Debuger/Debugger.cs
@@ -1,6 +1,56 @@
-﻿namespace WebApplication4.Debuger;
+﻿using System;
+using System.IO;
+using DebugerApp;
+using System.Collections.Generic;
+using System.Text.Json;
+using WebApplication4.Debuger;
 
-public class Debugger
+namespace DebugerApp;
+
+public class Debuger
 {
-    
+    public Debugger load()
+    {
+        var json  = File.ReadAllText("DebuggerSettings.json");
+        Debugger file = JsonSerializer.Deserialize<Debugger>(json); 
+        return file;
+    }
+
+    public (List<string> name, List<string> status) DebuggerSettings()
+    {
+        var test = load();
+        
+        List<string> Debuggername = new List<string>();
+        List<string> DebuggerStatus = new List<string>();
+        
+        foreach (var i in test.DebuggerInfo)
+        {
+            var DebuggerNameString = i.DebuggerSelection.ToString();
+            var DebuggerStatusString = i.DebuggerStatus.ToString();
+        
+            DebuggerStatus.Add(DebuggerStatusString);
+            Debuggername.Add(DebuggerNameString);
+        }
+        
+        return (Debuggername, DebuggerStatus);
+    }
+
+    public void Debugger(int Selection, string Item)
+    {
+        var test = DebuggerSettings();
+        var Debuggername = test.name;
+        var DebuggerStatus = test.status;
+        var DebuggernameInt =  Debuggername.ConvertAll(int.Parse);
+        var DebuggerStatusBool = DebuggerStatus.ConvertAll(bool.Parse);
+
+        if (DebuggernameInt.Contains(Selection))
+        {
+            var index = DebuggernameInt.IndexOf(Selection);
+            var DebuggerStatusInt = DebuggerStatusBool[index];
+            if (!false == DebuggerStatusInt)
+            {
+                Console.WriteLine(Item);
+            }
+        }
+    }
 }
---
+++ WebApplication4/Debuger/GetDebugerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication4.Debuger;
+
+public class Debugger
+{
+    public required List<DebuggerInfo> DebuggerInfo { get; init; }
+        
+}
+
+public class DebuggerInfo
+{
+    public required int DebuggerSelection { get; init; }
+    public required bool DebuggerStatus {get; init; }
+}
