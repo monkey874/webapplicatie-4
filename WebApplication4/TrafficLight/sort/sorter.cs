db015648e8d14f037b950ca26f923e88a59e7f4e
rickcodemake45 <504848@student.firda.nl>
2026-05-23 09:14:53 +0200

    Update: sorter.cs heb ik nagekeken op code kwaliteit en die is nu klaar.

--- WebApplication4/TrafficLight/sort/sorter.cs
+++ WebApplication4/TrafficLight/sort/sorter.cs
@@ -1,108 +1,96 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 using System.Text.Json;
 using WebApplication4.TrafficLight.models;
 
+
 namespace WebApplication4.TrafficLight.sort;
 
-public class sorter
+public class Sorter
 {
-    public (List<string> trafficLightNames, List<string> volgordeTrafficeLight) Laod(object incomingJsonobject)
+    public static (List<string> trafficLightNames, List<string> volgordeTrafficeLight) Laod(object incomingJsonobject)
     {
         var incomingjsonobject = incomingJsonobject.ToString();
-        RootGetMessages data = JsonSerializer.Deserialize<RootGetMessages>(incomingjsonobject);
-
-
-        List<string> typeVehicle = new List<string>();
-        List<string> trafficeLightId = new List<string>();
-        List<string> expectedArrival = new List<string>();
-
-
-        foreach (var i in data.Messages)
-        {
-            typeVehicle.Add(i.type);
-            trafficeLightId.Add(i.message.Id);
-            expectedArrival.Add(i.message.ExpectedArrival);
-        }
-
-
-        var defineClass = new vehiclePriority();
-
+        Debug.Assert(incomingjsonobject != null, nameof(incomingjsonobject) + " != null");
+        var data = JsonSerializer.Deserialize<RootGetMessages>(incomingjsonobject);
         
-        foreach (var i in (typeVehicle))
-        {
-            defineClass.checkVehiclePriority(i);
-            Console.WriteLine(i);
-        }
 
-        string[] test = trafficeLightId.ToArray();
-        List<int> aantalvechilesfortrafficeLight = new List<int>();
-        List<string> trafficeLightName = new List<string>();
+        List<string> typeVehicle = [];
+        List<string> trafficeLightId = [];
+   
+        if (data?.messages != null)
+            foreach (var i in data.messages)
+            {
+                typeVehicle.Add(i.type);
+                trafficeLightId.Add(i.message.id);
+            }
+
+
+        var test = trafficeLightId.ToArray();
+        List<int> aantalvechilesfortrafficeLight = [];
+        List<string> trafficeLightName = [];
         foreach (var i in test)
         {
-            string searchverb = i;
-            int aantal = test.Count(w => w == searchverb);
-            if (!trafficeLightName.Contains(searchverb))
-            {
-                aantalvechilesfortrafficeLight.Add(aantal);
-                trafficeLightName.Add(searchverb);
-            }
+            var searchverb = i;
+            var aantal = test.Count(w => w == searchverb);
+            if (trafficeLightName.Contains(searchverb)) continue;
+            aantalvechilesfortrafficeLight.Add(aantal);
+            trafficeLightName.Add(searchverb);
         }
 
-        List<string> volgordeTrafficeLight = new List<string>();
+        List<string> volgordeTrafficeLight = [];
         while (aantalvechilesfortrafficeLight.Count > 0)
         {
-            int n = aantalvechilesfortrafficeLight.Max();
-            int index = Array.IndexOf(aantalvechilesfortrafficeLight.ToArray(), n);
-            string trafficlight = trafficeLightName[index];
+            var n = aantalvechilesfortrafficeLight.Max();
+            var index = Array.IndexOf(aantalvechilesfortrafficeLight.ToArray(), n);
+            var trafficlight = trafficeLightName[index];
             aantalvechilesfortrafficeLight.Remove(n);
             trafficeLightName.RemoveAt(index);
             volgordeTrafficeLight.Add(trafficlight);
         }
-
-        var loader1 = new JsonLoader.JsonLoader();
-        var relation = loader1.LoadJsonFiles("relation.json");
-        RootRelationshipTrafficeLight jsonfile = JsonSerializer.Deserialize<RootRelationshipTrafficeLight>(relation);
-
-
         
-
-
+        var relation = JsonLoader.JsonLoader.LoadJsonFiles("relation.json");
+        var jsonfile = JsonSerializer.Deserialize<RootRelationshipTrafficeLight>(relation);
+        
         List<string> trafficLightNames = [];
 
+        if (jsonfile?.RelationshipTrafficLights == null) return (trafficLightNames, volgordeTrafficeLight);
         foreach (var trafficLightRelationship in jsonfile.RelationshipTrafficLights)
         {
             var nameTrafficLightDouble = trafficLightRelationship.TrafficLight;
-            var nameTrafficLight = nameTrafficLightDouble.ToString();
+            var nameTrafficLight = nameTrafficLightDouble.ToString(CultureInfo.InvariantCulture);
             trafficLightNames.Add(nameTrafficLight.Replace(",", "."));
         }
-
         return (trafficLightNames, volgordeTrafficeLight);
-
     }
     
-    public (List<double>trafficLightsNames, List<int>TrafficLightGreenTime, List<double>trafficLightsRelationshipOff, List<double> trafficeLightsRelationshipOn) generatorSort(object Jsonfile, string trafficid){
+    public static (List<double>trafficLightsNames, List<int>TrafficLightGreenTime, List<double>trafficLightsRelationshipOff, List<double> trafficeLightsRelationshipOn) GeneratorSort(object jsonFile, string trafficid){
         
         List<double> trafficLightsNames = [];
         List<int> trafficLightGreenTime = [];
         List<double> trafficeLightsRelationshipOn = [];
         List<double> trafficLightsRelationshipOff = [];
-
-        var data = Laod(Jsonfile);
         
-        var loader = new JsonLoader.JsonLoader();
-        var relation = loader.LoadJsonFiles("relation.json");
+        var relation = JsonLoader.JsonLoader.LoadJsonFiles("relation.json");
         var jsonfile = JsonSerializer.Deserialize<RootRelationshipTrafficeLight>(relation);
         
 
         var group = jsonfile?.RelationshipTrafficLights.FirstOrDefault(x => x.TrafficLight == double.Parse(trafficid, CultureInfo.InvariantCulture));
-            
-    
+
+
+        if (group == null)
+            return (
+                trafficLightsNames,
+                trafficLightGreenTime,
+                trafficLightsRelationshipOff,
+                trafficeLightsRelationshipOn
+            );
         trafficLightsNames.Add(group.TrafficLight);
         trafficLightGreenTime.Add(group.TrafficLightGreenTime);
         trafficeLightsRelationshipOn.AddRange(group.RelationshipOn);
         trafficLightsRelationshipOff.AddRange(group.RelationshipOff);
-            
-        
+
+
         return (
             trafficLightsNames, 
             trafficLightGreenTime,
